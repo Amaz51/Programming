777942cefae39d373d2b96daeaf7c76effc9d5b4
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-06-26 09:01:45 +0500

    Add files via upload

--- Source1.cpp
+++ Source1.cpp
@@ -1,275 +1,194 @@
-#include<iostream>
-using namespace std;
-
-
-template <typename T>
-class SortedSet {
-private:
-	struct node {
-		T data;
-		node* next;
-		node(const T& val) {
-			data = val;
-			next = NULL;
-		}
-	};
-	node* head;
-	node* tail;
-public:
-	//Q1
-	//construtor
-	SortedSet() {
-		head = NULL;
-		tail = NULL;
-	}
-	//INSERTION
-	void insert(T const data)
-	{
-		node* neww = new node(data);
-		if (!head) {
-			head = neww;
-			tail = neww;
-			return;
-		}
-		node* temp = head;
-		while (temp) {
-			int h = head->data;
-			int t = tail->data;
-			int tempp = temp->data;
-			if (h > neww->data)
-			{
-				neww->next = head;
-				head = neww;
-				return;
-			}
-			if (t < neww->data)
-			{
-				tail->next = neww;
-				tail = neww;
-			}
-			if (tempp < neww->data && temp->next && neww->data < temp->next->data)
-			{
-				neww->next = temp->next;
-				temp->next = neww;
-				return;
-			}
-			if (!(temp->next))
-			{
-				tail = temp;
-			}
-			temp = temp->next;
-		}
-	}
-
-	//DELETION
-	void Delete(int const ind)
-	{
-		int k = 0;
-		node* temp = head;
-		node* todel;
-		while (temp)
-		{
-			if (k == ind && head == temp) {
-				head = temp->next;
-				delete temp;
-				temp = NULL;
-				return;
-			}
-			if (k + 2 == ind && tail == temp->next) {
-				tail = temp;
-				temp = temp->next;
-				tail->next = NULL;
-				delete temp;
-				temp = NULL;
-				return;
-			}
-			if (k + 1 == ind) {
-				todel = temp->next;
-				temp->next = temp->next->next;
-				delete todel;
-				todel = NULL;
-			}
-			temp = temp->next;
-			k++;
-		}
-	}
-	//UNION
-	void Union(SortedSet<T> const& otherSet)
-	{
-		node* t1 = head;
-		node* t2 = otherSet.head;
-		node* newHead = nullptr;
-		node* curr = nullptr;
-
-		while (t1 || t2)
-		{
-			node* newNode = NULL;
-
-			if (!t1) {
-				newNode = new node(t2->data);
-				t2 = t2->next;
-			}
-			else if (!t2) {
-				newNode = new node(9t1->data);
-				t1 = t1->next;
-			}
-			else if (t1->data < t2->data) {
-				newNode = new node(t1->data);
-				t1 = t1->next;
-			}
-			else if (t1->data > t2->data) {
-				newNode = new node(t2->data);
-				t2 = t2->next;
-			}
-			else {
-				newNode = new node(t1->data);
-				t1 = t1->next;
-				t2 = t2->next;
-			}
-
-			if (!newHead) {
-				newHead = newNode;
-				curr = newHead;
-			}
-			else {
-				curr->next = newNode;
-				curr = curr->next;
-			}
-		}
-		while (head)
-		{
-			node* todel = head;
-			head = head->next;
-			delete todel;
-		}
-
-		head = newHead;
-		tail = curr;
-	}
-	//ROTATION
-	void Rotate(int k) {
-
-		node* curr = head;
-		for (int i = 0; i < k - 1; i++) {
-			curr = curr->next;
-		}
-		tail->next = head;
-		head = curr->next;
-		curr->next = NULL;
-		tail = curr;
-	}
-	//REVERSE
-	void reverse() {
-		if (head == nullptr || head->next == nullptr) { return; }
-		node* prev = NULL;
-		node* curr = head;
-		node* nextptr = NULL;
-
-		while (curr != NULL) {
-			nextptr = curr->next;
-			curr->next = prev;
-			prev = curr;
-			curr = nextptr;
-		}
-		head = prev;
-	}
-	//PRINT
-	void display() {
-		node* curr = head;
-		while (curr) {
-			cout << curr->data << "->";
-			curr = curr->next;
-		}cout << "NULL" << endl;
-	}
-	//DESTRUCTOR
-	~SortedSet()
-	{
-		node* temp = head;
-		while (temp)
-		{
-			node* todel = temp;
-			temp = temp->next;
-			delete todel;
-		}
-		head = NULL;
-		tail = NULL;
-	}
-};
-
-
-
-int main()
-{		//Insertion
-	SortedSet<int> list1;
-	cout << "\t\t\t\t\t\t" << "Question 1 " << '\t' << endl;
-	list1.insert(3);
-	list1.insert(20);
-	list1.insert(50);
-	list1.insert(30);
-	list1.insert(10);
-	list1.insert(25);
-	list1.insert(40);
-	list1.insert(60);
-	list1.insert(70);
-	list1.insert(60);
-
-
-	cout << "\t\t\t" << "Insertion in List 1 " << '\t' << endl << endl;
-
-	//Displaying list 1
-	list1.display();
-	cout << endl;
-
-	//Deletion of some elements
-	list1.Delete(3);
-	list1.Delete(70);
-	list1.Delete(25);
-
-
-	cout << "\t\t\t" << "Deletion in list 1 " << '\t' << endl << endl;
-	list1.display();
-	cout << endl;
-
-	//Insertion in list2
-	SortedSet<int> list2;
-
-	list2.insert(75);
-	list2.insert(85);
-	list2.insert(55);
-	list2.insert(35);
-	list2.insert(15);
-	list2.insert(10);
-
-
-	cout << "\t\t\t" << "Insertion in List 2 " << '\t' << endl << endl;
-	list2.display();
-	cout << endl;
-
-	list1.Union(list2);
-	//UNION
-	cout << "\t\t\t" << "UNION of List 2 with List 1 " << '\t' << endl << endl;
-	list1.display();
-	cout << endl << "\t\t\t\t\t\t" << "Question 2 " << '\t' << endl << endl << "Enter the k nodes to be rotated : ";
-
-
-	int nnn;
-	cin >> nnn;
-	if (nnn < 0) {
-		cout << "Entered incorrect number.";
-	}
-	else {
-		cout << endl;
-		list1.Rotate(nnn);
-		cout << "\t\t\t" << "ROTATION" << '\t' << endl << endl;
-		list1.display();
-	}
-
-
-	list1.reverse();
-	cout << endl << "\t\t\t\t\t\t" << "Question 3 " << '\t' << endl << endl << "\t\t\t" << "REVERSE" << '\t' << endl << endl;
-	list1.display();
-
-
-	cout << endl << "\t\t\t" << "Displaying B " << '\t' << endl << endl;
-	list2.display();
-}
+//#include <iostream>
+//
+//class IntervalHeap {
+//private:
+//    int hsize;       // Number of elements in heap
+//    int maxsize;     // Size of the array
+//    int** h;         // Array to store the lower and higher ends of closed intervals
+//
+//public:
+//    // Constructor
+//    IntervalHeap(int s = 100) {
+//        maxsize = s;
+//        hsize = 0;
+//        h = new int* [2];
+//        h[0] = new int[maxsize];  // Store the lower end of the closed interval
+//        h[1] = new int[maxsize];  // Store the higher end of the closed interval
+//    }
+//
+//    // Destructor
+//    ~IntervalHeap() {
+//        delete[] h[0];
+//        delete[] h[1];
+//        delete[] h;
+//    }
+//
+//    // Function to insert an element into the interval heap
+//    void Insert(int d) {
+//        // Ensure there is space in the interval heap
+//        if (hsize < maxsize) {
+//            // If the number of elements is even, create a new node
+//            if (hsize % 2 == 0) {
+//                // New node with both ends equal to the data element
+//                h[0][hsize] = d;
+//                h[1][hsize] = d;
+//
+//                // Restore the heap properties by adjusting the elements if needed
+//                int parent = (hsize - 1) / 2;
+//                int current = hsize;
+//
+//                // Swap elements to maintain min heap property for left ends
+//                while (current > 0 && h[0][parent] > h[0][current]) {
+//                    std::swap(h[0][parent], h[0][current]);
+//                    current = parent;
+//                    parent = (current - 1) / 2;
+//                }
+//
+//                // Swap elements to maintain max heap property for right ends
+//                while (current > 0 && h[1][parent] < h[1][current]) {
+//                    std::swap(h[1][parent], h[1][current]);
+//                    current = parent;
+//                    parent = (current - 1) / 2;
+//                }
+//
+//                ++hsize;
+//            }
+//            else {
+//                // The last node is not full, update its higher end
+//                h[1][hsize - 1] = d;
+//                ++hsize;
+//            }
+//        }
+//        else {
+//            std::cout << "Interval heap is full. Cannot insert element." << std::endl;
+//        }
+//    }
+//
+//    // Function to update an element in the interval heap
+//    void Update(int index, int newElement) {
+//        // Ensure the index is valid
+//        if (index >= 0 && index < hsize) {
+//            // Update the element at the specified index
+//            h[0][index] = newElement;
+//            h[1][index] = newElement;
+//
+//            // Maintain heap properties by adjusting the elements if needed
+//            int current = index;
+//
+//            // Swap elements to maintain min heap property for left ends
+//            while (current > 0 && h[0][(current - 1) / 2] > h[0][current]) {
+//                std::swap(h[0][(current - 1) / 2], h[0][current]);
+//                current = (current - 1) / 2;
+//            }
+//
+//            // Swap elements to maintain max heap property for right ends
+//            while (current > 0 && h[1][(current - 1) / 2] < h[1][current]) {
+//                std::swap(h[1][(current - 1) / 2], h[1][current]);
+//                current = (current - 1) / 2;
+//            }
+//        }
+//        else {
+//            std::cout << "Invalid index for update." << std::endl;
+//        }
+//    }
+//
+//    // Function to delete an element from the interval heap
+//    void Delete(int index) {
+//        // Ensure the index is valid
+//        if (index >= 0 && index < hsize) {
+//            // Swap the element at the specified index with the last element
+//            std::swap(h[0][index], h[0][hsize - 1]);
+//            std::swap(h[1][index], h[1][hsize - 1]);
+//
+//            // Adjust the heap size
+//            --hsize;
+//
+//            // Maintain heap properties by adjusting the elements if needed
+//            int current = index;
+//
+//            // Swap elements to maintain min heap property for left ends
+//            while (current < hsize) {
+//                int leftChild = 2 * current + 1;
+//                int rightChild = 2 * current + 2;
+//                int minIndex = current;
+//
+//                if (leftChild < hsize && h[0][leftChild] < h[0][minIndex]) {
+//                    minIndex = leftChild;
+//                }
+//
+//                if (rightChild < hsize && h[0][rightChild] < h[0][minIndex]) {
+//                    minIndex = rightChild;
+//                }
+//
+//                if (minIndex != current) {
+//                    std::swap(h[0][current], h[0][minIndex]);
+//                    current = minIndex;
+//                }
+//                else {
+//                    break;
+//                }
+//            }
+//
+//            // Swap elements to maintain max heap property for right ends
+//            current = index;
+//
+//            while (current < hsize) {
+//                int leftChild = 2 * current + 1;
+//                int rightChild = 2 * current + 2;
+//                int maxIndex = current;
+//
+//                if (leftChild < hsize && h[1][leftChild] > h[1][maxIndex]) {
+//                    maxIndex = leftChild;
+//                }
+//
+//                if (rightChild < hsize && h[1][rightChild] > h[1][maxIndex]) {
+//                    maxIndex = rightChild;
+//                }
+//
+//                if (maxIndex != current) {
+//                    std::swap(h[1][current], h[1][maxIndex]);
+//                    current = maxIndex;
+//                }
+//                else {
+//                    break;
+//                }
+//            }
+//        }
+//        else {
+//            std::cout << "Invalid index for deletion." << std::endl;
+//        }
+//    }
+//
+//    // Function to display the current state of the interval heap
+//    void Display() {
+//        std::cout << "Interval Heap:" << std::endl;
+//        for (int i = 0; i < hsize; ++i) {
+//            std::cout << "(" << h[0][i] << "," << h[1][i] << ") ";
+//        }
+//        std::cout << std::endl;
+//    }
+//};
+//
+//int main() {
+//    // Example usage
+//    IntervalHeap intervalHeap;
+//
+//    // Insert elements into the interval heap
+//    intervalHeap.Insert(2);
+//    intervalHeap.Insert(3);
+//    intervalHeap.Insert(4);
+//    intervalHeap.Insert(5);
+//
+//    // Display the interval heap
+//    intervalHeap.Display();
+//
+//    // Update an element
+//    intervalHeap.Update(2, 10);
+//    intervalHeap.Display();
+//
+//    // Delete an element
+//    intervalHeap.Delete(1);
+//    intervalHeap.Display();
+//
+//    return 0;
+//}
