9e08d7efd6cef52092b8238b9be781cf0d1958fa
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-06-30 14:06:19 +0500

    Add files via upload

---
+++ Global.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<Windows.h>
+std::string a = "_______________________________________________________________ ";
+std::string b = "_______________________________________________________________ ";
+std::string p1l = " _____";
+std::string b1l = " _____";
+std::string p2l = " _____";
+std::string b2l = " _____";
+char p1a = ' ', p1b = ' ', p1c = ' ', p1d = ' ', p2a = ' ', p2b = ' ', p2c = ' ', p2d = ' ';
+char end1a = ' ', end1b = ' ', end1c = ' ', end1d = ' ', end2a = '_', end2b = '_', end2c = '_', end2d = '_';
+std::string player1_name, player2_name;
+int roll1, roll2;
+bool roll1s, roll2s, endtokenthisturn, beathisturn;
+char choice1;
+int choice2;
+bool roll1pos1a, roll1pos1b, roll1pos1c, roll1pos1d, roll2pos1a, roll2pos1b, roll2pos1c, roll2pos1d;
+bool roll1pos2a, roll1pos2b, roll1pos2c, roll1pos2d, roll2pos2a, roll2pos2b, roll2pos2c, roll2pos2d;
+std::string same = "         _                             _            ";
