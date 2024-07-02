ad77c337c38cc7aa8a368507a21bf285c5afd5cf
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-02 11:23:56 +0500

    Add files via upload

--- 22L6837.cpp
+++ 22L6837.cpp
@@ -1,209 +1,180 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
-
-template <typename T>
-class Queue {
+class MyString
+{
 private:
-    struct Node {
-        T data;
-        Node* next;
-        Node(const T& value) {
-            data = value;
-            next = NULL;
-        }
-    };
-    Node* front;
-    Node* rear;
+	char* str;
+	int length;						
+	char GetStringFromBuffer;		
+	char Concatenate;
 
 public:
-    Queue() {
-        front = NULL;
-        rear = NULL;
-    }
+	
+	MyString operator+(const MyString);
+	MyString& operator=(const MyString&);
+	bool operator<(MyString);
 
-    ~Queue() {
-        while (!isEmpty()) {
-            dequeue();
-        }
-    }
+	MyString()
+	{
+		str = NULL;
+		length = 1;
+	}
+	char& operator[](int);
+	MyString operator()(int, int);
+	friend ostream& operator<<(ostream& COUT, const MyString& myString)
+	{
+		if (myString.str != nullptr) {
+			COUT << myString.str;
 
-    void enqueue(const T& value) {
-        Node* newNode = new Node(value);
-        if (isEmpty()) {
-            front = rear = newNode;
-        }
-        else {
-            rear->next = newNode;
-            rear = newNode;
-        }
-    }
+		}
+		return COUT;
+	}
+	friend istream& operator>> (istream& CIN, MyString& Mystring)
+	{
 
-    void dequeue() {
-        if (!isEmpty()) {
-            Node* temp = front;
-            front = front->next;
-            delete temp;
-        }
-        else {
-
-            cout << "Queue is empty ." << endl;
-        }
-    }
-
-    T peek() const {
-        if (!isEmpty()) {
-            return front->data;
-        }
-        else {
-            cout << "Queue is empty" << endl;
-        }
-    }
-
-    bool isEmpty() const {
-        return front == nullptr;
-    }
+		char temp[1000];
+		int i = 0;
+		char ch;
+		while ((ch = CIN.get()) != '\n')
+		{
+			temp[i++] = ch;
+		}
+		temp[i] = '\0';
+		Mystring.length = i + 1; // add 1 for the null character
+		Mystring.str = new char[Mystring.length];
+		for (int j = 0; j < i; j++)
+		{
+			Mystring.str[j] = temp[j];
+		}
+		Mystring.str[i] = '\0';
+		return CIN;
+	}
+	bool operator!() {
+		return (str == nullptr || length <= 1);
+	}
+	void myStringCopy(char* dest, const char* src)
+	{
+		int i = 0;
+		while (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	//~MyString() {
+	//    delete[] str; // release the memory allocated for the string
+	//    length = 0; // set the length to 0 to indicate that the string is no longer valid
+	//}
 };
-
-class Book {
-    string isbn;
-    string title;
-    string author;
-    bool available;
-public:
-    Book(const string& isb, const string& tit, const string& aut) {
-        isbn = isb;
-        title = tit;
-        author = aut;
-        available = 1;
-    }
-    Book(){}
-
-
-    string getISBN() const { return isbn; }
-    string getTitle() const { return title; }
-    string getAuthor() const { return author; }
-    bool isAvailable() const { return available; }
-
-    void reserve() { available = false; }
-    void returnBook() { available = true; }
-};
-
-class Patron {
-    string id;
-    string name;
-    Book* reservedBook = nullptr;
-public:
-    Patron(){}
-    Patron(const string& ID, const string& Name) {
-        name = Name;
-        id = ID;
-        reservedBook = NULL;
-    }
-
-
-    string getID() const { return id; }
-    string getName() const { return name; }
-    Book* getReservedBook() { return reservedBook; }
-
-    void reserveBook(Book* book) {
-        if (book->isAvailable() && reservedBook == nullptr) {
-            reservedBook = book;
-            reservedBook->reserve();
-        }
-    }
-
-    void returnReservedBook() {
-        if (reservedBook) {
-            reservedBook->returnBook();
-            reservedBook = nullptr;
-        }
-    }
-};
-
-class Library {
-
-    Queue<Book> books;
-    Queue<Patron> patrons;
-
-
-    Patron* findPatron(const string& id) {
-        Queue<Patron> tempq = patrons;
-        while (!tempq.isEmpty()) {
-            Patron patron = tempq.peek();
-            if (patron.getID() == id) {
-                return &patron;
-            }
-            tempq.dequeue();
-        }
-        return nullptr;
-    }
-
-    Book* findBook(const string& isbn) {
-        Queue<Book> tempq = books;
-        while (!tempq.isEmpty()) {
-            Book book = tempq.peek();
-            if (book.getISBN() == isbn) {
-                return &book;//extracting and checking one by one
-            }
-            tempq.dequeue();
-        }
-        return nullptr;
-    }
-
-
-public:
-    void addBook(const string& isbn, const string& title, const string& author) {
-        books.enqueue(Book(isbn, title, author));//adding the book
-    }
-
-    void addPatron(const string& id, const string& name) {
-        patrons.enqueue(Patron(id, name));//adding patron
-    }
-
-    void reserveBook(const string& patronID, const string& bookISBN) {
-        Patron* patron = findPatron(patronID);
-        Book* book = findBook(bookISBN);
-        if (patron && book) {
-            patron->reserveBook(book);
-        }
-    }
-
-    void returnBook(const string& patronID) {
-        Patron* patron = findPatron(patronID);
-        if (patron) {
-            patron->returnReservedBook();
-        }
-    }
-
-    void displayBooks() {
-        Queue<Book> tempq = books;
-        while (!tempq.isEmpty()) {
-            Book book = tempq.peek();
-            cout << "Title: " << book.getTitle() << ", Author: " << book.getAuthor();
-            if (book.isAvailable()) {
-                cout << " Available" << endl;
-            }
-            else {
-                cout << " Reserved" << endl;
-            }
-            tempq.dequeue();
-        }
-    }
-
-};
-
-int main() {
-    Library lib;
-    lib.addBook("123456", "Sample Book 1", "Author A");
-    lib.addBook("789012", "Sample Book 2", "Author B");
-    lib.addPatron("P001", "John Doe");
-    lib.addPatron("P002", "Jane Smith");
-
-    lib.reserveBook("P001", "123456");
-    lib.reserveBook("P002", "789012");
-
-    lib.displayBooks();
-
-    return 0;
+//--------------------------Add your code here----------------------
+char& MyString ::operator[](int i)
+{
+	return str[i];
 }
+MyString MyString::operator()(int start, int end)
+{
+	int newLength = end - start + 2;
+	char* newStr = new char[newLength];
+	for (int i = 0; i < newLength - 1; i++) {
+		newStr[i] = str[start + i];
+	}
+	newStr[newLength - 1] = '\0';
+
+	MyString result;
+	result.length = newLength;
+	result.str = newStr;
+
+	return result;
+}
+
+MyString MyString::operator+(MyString other)
+{
+	MyString result;
+	result.length = length + other.length - 1; // subtracting 1 as the last character is a null character
+	result.str = new char[result.length];
+	myStringCopy(result.str, str);
+	myStringCopy(result.str + length - 1, other.str);
+	return result;
+}
+
+bool MyString:: operator<(MyString other)
+{
+	int i = 0;
+	while (str[i] != '\0' && other.str[i] != '\0')
+	{
+		if (str[i] < other.str[i])
+			return true;
+		else if (str[i] > other.str[i])
+			return false;
+		i++;
+	}
+	if (str[i] == '\0' && other.str[i] != '\0')
+		return true;
+	return false;
+}
+
+MyString& MyString::operator=(const MyString& other)
+{
+	if (this != &other)
+	{
+		delete[] str;
+		length = other.length;
+		str = new char[length];
+		myStringCopy(str, other.str);
+	}
+	return *this;
+}
+
+
+//-------------DO_NOT_CHANGE REGION starts below---------------------
+int main()
+{
+	MyString str1, str2, str3, str4; //Default constructor will make a string of lenght 1 having null character only i.e.empty string
+	if (!str1)
+	{
+		cout << "String 1 is Empty.\n";
+		cout << "Str 1 = " << str1 << endl << endl << endl;
+
+	}
+	cout << "Enter String 1:\t";
+	cin >> str1;
+	cout << "Enter String 2:\t";
+	cin >> str2;
+	cout << "\n\n\nUser Entered:\n";
+	cout << "String 1 = " << str1 << endl;
+	cout << "String 2 = " << str2 << endl << endl << endl;
+	//What is following code testing?
+	cout << "Before str1 = str1; str1 = " << str1 << endl;
+	str1 = str1;
+	cout << "After str1 = str1, str1 = " << str1 << endl << endl << endl;
+	cout << "Before str4 = str3 = str1+str2\n";
+	cout << "str1 = " << str1 << endl;
+	cout << "str2 = " << str2 << endl;
+	cout << "str3 = " << str3 << endl;
+	cout << "str4 = " << str4 << endl;
+	str4 = str3 = str1 + str2;
+	cout << "\n\n\nAfter str4 = str3 = str1+str2\n";
+	cout << "str1 = " << str1 << endl;
+	cout << "str2 = " << str2 << endl;
+	cout << "str3 = " << str3 << endl;
+	cout << "str4 = " << str4 << endl;
+	cout << "\n\n\nEnter String 3:\t";
+	cin >> str3;
+	cout << "\n\n\nEnter String 4:\t";
+	cin >> str4;
+	cout << "\n\n\nstr3 = " << str3 << endl;
+	cout << "str4 = " << str4 << endl;
+	if (str3 < str4)
+		cout << "String 3 is Less than String 4.\n";
+	else
+		cout << "String 3 is NOT Less than String 4.\n";
+	MyString str5 = str1 + str2;
+	cout << "\n\n\nStr5:\t" << str5 << endl;
+	cout << "Str5[7]:\t" << str5[7] << endl; //Function Call: str5.operator[](7).
+	str5[7] = '$';
+	cout << "\n\nStr5:\t" << str5 << endl;
+	cout << "\n\n\nstr5(5, 10):\t" << str5(5, 10) << endl;// Substring of lenght 10	starting from index 5.Function Call str5.operator()(5, 10) Let the returned MyString or	char* leak
+}
+//-------------End of DO_NOT_CHANGE REGION---------------------
