316d1782f4fbae9975bb4bda8657c208ef6ac6ae
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-06 12:41:33 +0500

    Add files via upload

--- Q3.cpp
+++ Q3.cpp
@@ -1,35 +1,19 @@
-#include<iostream>
-using namespace std;
-int main() {
-	int j, n, arr[100], key;
-	cout << "Enter the size of array : ";
-	cin >> n;
-	cout << "Enter the array elements : ";
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	for (int i = 2; i < n; i++) {
-		j = i - 2;
-		key = arr[i];
-		if ((i + 1) % 2 == 1) {
-			while (key >= arr[j] && j >= 0) {
-				arr[j + 2] = arr[j];
-				j = j - 2;
-
-			}arr[j + 2] = key;
-		}
-		else {
-			while (key <= arr[j] && j >= 0) {
-				arr[j + 2] = arr[j];
-				j = j - 2;
-
-			}arr[j + 2] = key;
-		}
-	}
-	cout << "The sorted array is : ";
-	for (int k = 0; k < n; k++) {
-		cout << arr[k] << " ";
-	}
-	cout << endl;
-	
-}
+//#include<iostream>
+//using namespace std;
+//int len(int &leng,char a[]) {
+//	
+//	for (int i = 0; a[i] != '\0'; i++) {
+//		leng++;
+//
+//	}return leng;
+//}
+//int main() {
+//		char a[100];
+//		int leng = 0;
+//	cout << "Enter a line: ";
+//	cin.getline(a,50);
+//	len(leng, a);
+//	for (int i = leng-1; i >= 0; i--) {
+//		cout << a[i];
+//	}
+//}
