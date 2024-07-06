5f6383041ed72b6c58463b736df155d99bbbd59e
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-06 12:42:11 +0500

    Add files via upload

--- Q2.cpp
+++ Q2.cpp
@@ -1,84 +1,10 @@
-#include<iostream>
-using namespace std;
-class BasicShape {
-	double area;
-	double shapearea;
-public:
-	double getarea() {
-		return area;
-	}
-	void setarea(double ar) {
-		area = ar;
-	}
-	
-	virtual void calcArea() {
-
-	}
-};
-
-class Circle:public BasicShape {
-	long int centerX;
-	long int centerY;
-	double radius;
-	
-public:
-	Circle(long int x,long int y,double rad) {
-		centerX = x;
-		centerY = y;
-		radius = rad;
-		calcArea();
-	}
-	long int getCenterX() {
-		return centerX;
-	}
-	long int getCenterY() {
-		return centerY;
-	}
-	void calcArea() {
-		double a = 3.14159 * radius * radius;
-		BasicShape::setarea(a);
-	}
-};
-class Rectangle :public BasicShape {
-	long int length;
-	long int width;
-
-public:
-	Rectangle(long int len, long int wid) {
-		length = len;
-		width = wid;
-		calcArea();
-	}
-	long int getwidth() {
-		return width;
-	}
-	long int getlenght() {
-		return length;
-	}
-	void calcArea() {
-		double a = length*width;
-		BasicShape::setarea(a);
-	}
-
-};
-
-
-int main() {
-	int x, y;
-	double r;
-	cout << "Enter X-Coordinate of the circle : ";
-	cin >> x;
-	cout << "Enter Y-Coordinate of the circle : ";
-	cin >> y;
-	cout << "Enter Radius of the circle : ";
-	cin >> r;
-	Circle c(x,y,r);
-	cout << "Area of Circle is : " << c.getarea()<<endl<<endl;
-	long int xx, yy;
-	cout << "Enter length of rectangle : ";
-	cin >> xx;
-	cout << "Enter width of rectangle : ";
-	cin >> yy;
-	Rectangle rr(xx,yy);
-	cout << "Area of Rectangle is : " << rr.getarea()<<endl;
-}
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	char a[100];
+//	cout << "Enter a line to split";
+//	cin.getline(a, 50);
+//	for (int i = 0; a[i] != '\0'; i++) {
+//		cout << a[i] << endl;;
+//	}
+//}
