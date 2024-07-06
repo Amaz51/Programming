3f028f369ff628ca36705e4ac0691516fc5f61ca
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-06 12:40:58 +0500

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,35 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int len(int &leng,char a[]) {
+//	
+//	for (int i = 0; a[i] != '\0'; i++) {
+//		leng++;
+//
+//	}return leng;
+//}
+//
+//int main(){
+//	char a[100], b[100];
+//	int leng = 0,le=0;
+//	cout << "Enter string 1 : ";
+//	cin.getline(a, 50);
+//	cout << "Enter string 2 : ";
+//	cin.getline(b, 50);
+//	len(leng, a);
+//	len(le, a);
+//	if (leng != le) {
+//		cout << "These are not equal.";
+//	}
+//	else if(leng==le){
+//		bool f = 0;
+//		for (int i = 0; a[i] != '\0'; i++) {
+//			
+//			if (a[i] == b[i]) {
+//				f = 1;
+//			}
+//		}if (f == 1) {
+//				cout << "string are equal";
+//			}
+//	}
+//}
