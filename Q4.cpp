7b748577b573700030c2185f712d31b92ffebeca
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-06 12:39:01 +0500

    Add files via upload

--- Q4.cpp
+++ Q4.cpp
@@ -1,63 +1,13 @@
-//#include<iostream>
-//using namespace std;
-//void sorttt(int a[],int b) {
-//	for (int i = 0; i < b; i++) {
-//		for (int j = i + 1; j < b; j++) {
-//			if (a[j] < a[i]) {
-//				int temp = a[i];
-//				a[i] = a[j];
-//				a[j] = temp;
-//			}
-//		}
-//	}cout << endl;
-//	cout << "The sorted array is : ";
-//	for (int h = 0; h < b; h++) {
-//		cout << a[h] << " ";
-//	}
-//	cout << endl;
-//}
-//
-//void insertt(int b[],int &c) {
-//	cout << "Enter the number to insert in sorted array : ";
-//	int num;
-//	cin >> num;
-//	b[c] = num;
-//	cout << endl;
-//	c++;
-//}
-//void deletee(int c[],int &d) {
-//	int post;
-//	cout << "Enter the index to delete in sorted array : ";
-//	cin >> post;
-//	for (int i = post; i < d; i++) {
-//
-//		c[i] = c[i + 1];
-//
-//	}
-//	cout << endl;
-//	d--;
-//}
-//int main()
-//{
-//	int arr[100], ser, n;
-//	cout << "Enter the size of array : ";
-//	cin >> n;
-//	cout << "Enter the array elements : ";
-//	for (int i = 0; i < n; i++) {
-//		cin >> arr[i];
-//	}
-//	sorttt(arr, n);
-//	cout << "Enter the number to search : ";
-//	cin >> ser;
-//	for (int i = 0; i < n; i++) {
-//		if (arr[i] == ser) {
-//			cout << "The number is at : " << i << endl;
-//		}
-//	}
-//		
-//	cout << endl;
-//	insertt(arr, n);
-//	deletee(arr, n);
-//	sorttt(arr, n);
-//	
-//}
+#include<iostream>
+using namespace std;
+int main(){
+	char st[100];
+	cout << "Enter the string: ";
+	cin.getline(st, 100);
+	int count = 0;
+	for (int i = 0; st[i] != '\0'; i++) {
+		if (st[i] == ' ') {
+			count++;
+		}
+	}cout << "The number of words are : "<<count+1;
+}
