16e8051675aa24e51156357feb2533f06c1a2c8b
Amaz Ahmed <121846782+Amaz51@users.noreply.github.com>
2024-07-07 22:49:44 +0500

    Add files via upload

--- Q3.cpp
+++ Q3.cpp
@@ -1,19 +1,59 @@
-//#include<iostream>
-//using namespace std;
-//int len(int &leng,char a[]) {
-//	
-//	for (int i = 0; a[i] != '\0'; i++) {
-//		leng++;
-//
-//	}return leng;
-//}
-//int main() {
-//		char a[100];
-//		int leng = 0;
-//	cout << "Enter a line: ";
-//	cin.getline(a,50);
-//	len(leng, a);
-//	for (int i = leng-1; i >= 0; i--) {
-//		cout << a[i];
-//	}
-//}
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define noofphilos 5
+
+sem_t forks[noofphilos];
+sem_t room_sem;
+
+void* philosopher(void* arg) {
+    int phil_id = *(int*)arg;
+    int l_fork = phil_id;
+    int r_fork = (phil_id + 1) % noofphilos;
+
+    while (1) {
+        printf("Philosopher %d is thinking.\n", phil_id);
+        sleep(2);
+
+        sem_wait(&room_sem); 
+
+        sem_wait(&forks[l_fork]);
+        sem_wait(&forks[r_fork]);
+
+        printf("Philosopher %d is eating.\n", phil_id);
+        sleep(3);
+
+        sem_post(&forks[l_fork]);
+        sem_post(&forks[r_fork]);
+
+        sem_post(&room_sem); 
+    }
+}
+
+int main() {
+    pthread_t phills[noofphilos];
+    int ids[noofphilos];
+
+    sem_init(&room_sem, 0, noofphilos - 1); 
+    for (int i = 0; i < noofphilos; i++) {
+        sem_init(&forks[i], 0, 1); 
+    }
+
+    for (int i = 0; i < noofphilos; i++) {
+        ids[i] = i;
+        pthread_create(&phills[i], NULL, philosopher, &ids[i]);
+    }
+
+    for (int i = 0; i < noofphilos; i++) {
+        pthread_join(phills[i], NULL);
+    }
+
+    sem_destroy(&room_sem);
+    for (int i = 0; i < noofphilos; i++) {
+        sem_destroy(&forks[i]); // Destroying semaphores for forks
+    }
+
+    return 0;
+}
